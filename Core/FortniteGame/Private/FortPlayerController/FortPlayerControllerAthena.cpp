20c8eecf68fa750c1bb95fefc28950b7196b37ff
PongoDev <90353497+PongooDev@users.noreply.github.com>
2026-06-20 17:46:38 +0100

    Update FortPlayerControllerAthena.cpp

--- Core/FortniteGame/Private/FortPlayerController/FortPlayerControllerAthena.cpp
+++ Core/FortniteGame/Private/FortPlayerController/FortPlayerControllerAthena.cpp
@@ -122,7 +122,7 @@ void AFortPlayerControllerAthena::ClientOnPawnDied_Implementation(AFortPlayerCon
 			}
 		}
 
-		if (Version::Fortnite_Version < 2.1) {
+		if ((Version::Fortnite_Version < 2.1 && Version::Fortnite_Version > 1.91) || Version::Fortnite_Version == 1.10 || Version::Fortnite_Version == 1.11) {
 			if (FortGameModeAthena->bAllowSpectateAfterDeath) {
 				APawn* PawnToSpectate = DeathReport.KillerPawn;
 				if (!PawnToSpectate) {
