1a3314ad8a2d21bdc07e03376584479cfa44a5dc
PongoDev <90353497+PongooDev@users.noreply.github.com>
2026-07-19 02:58:58 +0100

    Update FortGameModeAthena.cpp

--- Core/FortniteGame/Private/FortGameMode/FortGameModeAthena.cpp
+++ Core/FortniteGame/Private/FortGameMode/FortGameModeAthena.cpp
@@ -207,8 +207,6 @@ void AFortGameModeAthena::FinishWorldInitialization(AFortGameModeAthena* This, A
 
 		Log("Set Custom Supply Drop Class: " + SupplyDropClass->GetName().ToString());
 	}
-
-	GameState->OnRep_Aircraft();
 }
 
 void AFortGameModeAthena::AddToAlivePlayers(AFortPlayerControllerAthena* PC) {
